9ae21cc5608a37e637ac3c86b67671b6f9caa1e9
Ruben <grubina.men@gmail.com>
2024-02-13 18:37:01 +0300

    delete EditorService, role in User

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { EditorsModule } from './editors/editors.module';
 import { TgBotModule } from './tg-bot/tg-bot.module';
 
 @Module({
-  imports: [UsersModule, EditorsModule, TgBotModule],
+  imports: [UsersModule, TgBotModule],
   controllers: [AppController],
   providers: [AppService],
 })
